992ec920c512cb225ea9bdd777f4299d23e2e447
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2024-12-07 14:29:39 -0500

    Actualización 1.0

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <a href='https://inmedia-testing.vercel.app' className='logo'>
+                    <img src="https://inmedia.pe/Componentes/Header/Imagenes/logo_nav.png" alt="Logo de Inmedia Marketing Web"></img>
+                </a>
+
+                <p>Menú</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
